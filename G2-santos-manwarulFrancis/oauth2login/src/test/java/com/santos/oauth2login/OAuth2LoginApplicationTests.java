98f9efe645cd888618e7dd900ee5fdcd96ca6707
LAB13_USER ON WS33 <L13X13W33@citulab.net>
2025-02-19 12:32:28 +0800

    initial Spring Boot OAuth2 Login

---
+++ G2-santos-manwarulFrancis/oauth2login/src/test/java/com/santos/oauth2login/OAuth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.oauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
